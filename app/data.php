edeaf1927d8b6bf40cd4b335755e11e7b1d5994c
hellomaruf <marufahamed1110@gmail.com>
2026-03-08 14:25:44 +0600

    first commit

---
+++ app/data.php
@@ -0,0 +1,110 @@
+<?php
+$facebookPosts = [
+    [
+        "id" => 1,
+        "user" => [
+            "user_id" => 101,
+            "name" => "Maruf Ahamed",
+            "profile_image" => "https://randomuser.me/api/portraits/men/32.jpg"
+        ],
+        "caption" => "Nature is not a place to visit. It is home. 🌿",
+        "image" => "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+        "location" => "Bandarban, Bangladesh",
+        "privacy" => "public",
+        "likes" => 245,
+        "shares" => 12,
+        "created_at" => "2026-03-01 10:30:00",
+        "comments" => [
+            [
+                "comment_id" => 1,
+                "user" => "Rahim",
+                "text" => "Beautiful place!",
+                "time" => "2026-03-01 11:00:00"
+            ],
+            [
+                "comment_id" => 2,
+                "user" => "Karim",
+                "text" => "Amazing photo 😍",
+                "time" => "2026-03-01 11:10:00"
+            ]
+        ]
+    ],
+    [
+        "id" => 2,
+        "user" => [
+            "user_id" => 102,
+            "name" => "Sarah Ahmed",
+            "profile_image" => "https://randomuser.me/api/portraits/women/44.jpg"
+        ],
+        "caption" => "Enjoying the little moments in life. ✨",
+        "image" => "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+        "location" => "Dhaka, Bangladesh",
+        "privacy" => "friends",
+        "likes" => 180,
+        "shares" => 6,
+        "created_at" => "2026-03-02 14:20:00",
+        "comments" => [
+            [
+                "comment_id" => 3,
+                "user" => "Nusrat",
+                "text" => "So true ❤️",
+                "time" => "2026-03-02 15:00:00"
+            ]
+        ]
+    ],
+    [
+        "id" => 3,
+        "user" => [
+            "user_id" => 103,
+            "name" => "Tanvir Hasan",
+            "profile_image" => "https://randomuser.me/api/portraits/men/65.jpg"
+        ],
+        "caption" => "Coffee first, everything else later. ☕",
+        "image" => "https://images.unsplash.com/photo-1492724441997-5dc865305da7",
+        "location" => "Chattogram, Bangladesh",
+        "privacy" => "public",
+        "likes" => 320,
+        "shares" => 18,
+        "created_at" => "2026-03-03 09:10:00",
+        "comments" => []
+    ],
+    [
+        "id" => 4,
+        "user" => [
+            "user_id" => 104,
+            "name" => "Nusrat Jahan",
+            "profile_image" => "https://randomuser.me/api/portraits/women/12.jpg"
+        ],
+        "caption" => "Beach days are the best days. 🌊",
+        "image" => "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+        "location" => "Cox's Bazar, Bangladesh",
+        "privacy" => "public",
+        "likes" => 540,
+        "shares" => 40,
+        "created_at" => "2026-03-04 17:45:00",
+        "comments" => [
+            [
+                "comment_id" => 4,
+                "user" => "Imran",
+                "text" => "Wish I was there!",
+                "time" => "2026-03-04 18:00:00"
+            ]
+        ]
+    ],
+    [
+        "id" => 5,
+        "user" => [
+            "user_id" => 105,
+            "name" => "Sabbir Rahman",
+            "profile_image" => "https://randomuser.me/api/portraits/men/20.jpg"
+        ],
+        "caption" => "Chasing sunsets and dreams. 🌅",
+        "image" => "https://images.unsplash.com/photo-1472214103451-9374bd1c798e",
+        "location" => "Sylhet, Bangladesh",
+        "privacy" => "friends",
+        "likes" => 410,
+        "shares" => 25,
+        "created_at" => "2026-03-05 18:30:00",
+        "comments" => []
+    ]
+];
---
+++ autoload.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists(__DIR__ . '/app/data.php')) {
+   require_once __DIR__ . '/app/data.php';
+}
